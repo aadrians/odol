e3abba67522389fdb5788985389696072a282a1e
arya <arya.adriansyah@gmail.com>
2016-06-11 21:21:48 +0200

    create success page

--- src/main/java/nl/musihoven/HomePage.java
+++ src/main/java/nl/musihoven/HomePage.java
@@ -1,8 +1,9 @@
 package nl.musihoven;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
@@ -10,9 +11,22 @@ public class HomePage extends WebPage {
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
 
+		add(new Label("message", "Hello world!"));
 		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
 
 		// TODO Add your page's components here
+		Form<?> form = new Form<Void>("userForm"){
+			private static final long serialVersionUID = -1930410850159114718L;
 
+			@Override
+			protected void onSubmit(){
+				System.out.println("Form submitted");
+				PageParameters pageParameters = new PageParameters();
+				pageParameters.add("msg", "this is parameter value");
+				setResponsePage(SuccessPage.class, pageParameters);
+			}
+		};
+		add(form);
     }
+	
 }
